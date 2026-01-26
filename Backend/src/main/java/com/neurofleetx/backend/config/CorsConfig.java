f2cc55fc5c2176b7bd3c4d7478b1da2f0e13bbca
keerthisugupatinti02 <239x1a05l8@gprec.ac.in>
2026-01-26 23:17:07 +0530

    Updated url

--- Backend/src/main/java/com/neurofleetx/backend/config/CorsConfig.java
+++ Backend/src/main/java/com/neurofleetx/backend/config/CorsConfig.java
@@ -1,3 +1,7 @@
+package com.neurofleetx.backend.config; import org.springframework.context.annotation.Bean;
+ import org.springframework.context.annotation.Configuration;
+ import org.springframework.web.servlet.config.annotation.CorsRegistry;
+ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
